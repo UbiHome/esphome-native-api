60d4a3d7e10ad7044fd24c2d4096a08569b52fc0
DanielHabenicht <daniel-habenicht@outlook.de>
2025-07-12 21:41:00 +0200

    fix generator script

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -3,6 +3,12 @@ use std::fs::{self, OpenOptions};
 use std::io::Write;
 use std::path::Path;
 use std::env;
+const current_version: &str = "2025.4.1";
+const last_supplied_version: &str = "2025.2.1";
+
+fn get_package_name(version: &str) -> String {
+    format!("version_{}", version.clone()).replace(".", "_")
+}
 
 #[tokio::main]
 async fn main() {
@@ -34,7 +40,7 @@ async fn main() {
             continue;
         }
         // Only till this release:
-        if &release.tag_name == "2025.2.1" {
+        if &release.tag_name == last_supplied_version {
             break;
         }
 
@@ -80,13 +86,14 @@ async fn main() {
             println!("No content found in the API response.");
         }
 
-        let package_name = format!("version_{}", release.tag_name.clone()).replace(".", "_");
+        let package_name = get_package_name(&release.tag_name);
+        
         versions.push(package_name.clone());
         mod_file_content.push_str(&format!("#[cfg(feature = \"{}\")]\npub mod {};\n", package_name, package_name));
         let write_dir = format!("{}/{}", root_output_dir, package_name);
         let write_protos_dir = Path::new(write_dir.as_str());
         fs::create_dir_all(write_protos_dir).unwrap();
-
+        
         let mut config = prost_build::Config::new();
         // config.skip_debug(&["."]);
         config.default_package_filename(&package_name);
@@ -94,7 +101,7 @@ async fn main() {
         config.include_file("mod.rs");
         config.compile_protos(&[format!("{}/api.proto", dir)], &[dir]).unwrap();
     }
-
+    
     let mod_file = Path::new(root_output_dir).join("mod.rs");
     fs::write(mod_file, mod_file_content).unwrap();
 
@@ -105,7 +112,8 @@ async fn main() {
     .unwrap();
 
     file.write(b"\n[features]\n").unwrap();
-    file.write(format!("default = [\"{}\"]\n", versions.first().unwrap()).as_bytes()).unwrap();
+    let default_feature_flag = get_package_name(current_version);
+    file.write(format!("default = [\"{}\"]\n", default_feature_flag).as_bytes()).unwrap();
     
 
     for version in versions {
